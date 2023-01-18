d0c4900d206de2b22c0afd00d09f80ddac5d4993
Arihant Banthia <arihantbanthia911@gmailcom>
2023-01-18 12:40:39 +0530

    Add the Complete Feature.

--- web.py
+++ web.py
@@ -19,11 +19,17 @@ st.title("My To-Do App")
 st.subheader("This is my to-do wen App")
 st.write("This app is used to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index).strip('\n')
+        functions.set_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add a Todo......",
               on_change=add_todo,
               key="new_todo")
 # label is used to display the text just above the text box
 # while placeholder is used to display the bydefault text in text box
+st.session_state
